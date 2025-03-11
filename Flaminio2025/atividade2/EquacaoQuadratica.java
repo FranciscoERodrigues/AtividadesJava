d17982ec0fd3c0bf09979be30e32c21c7cb9aab3
Francisco E R Rodrigues <163360208+FranciscoERodrigues@users.noreply.github.com>
2025-03-10 21:14:33 -0300

    Add files via upload

---
+++ Flaminio2025/atividade2/EquacaoQuadratica.java
@@ -0,0 +1,33 @@
+package atividade2;
+
+import java.util.Scanner;
+
+
+public class EquacaoQuadratica {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite o valor de A da equação quadratica:");
+		float numero01 = sc.nextFloat();
+		System.out.println("Digite o valor de B da equação quadratica:");
+		double numero02 = sc.nextDouble();
+		System.out.println("Digite o valor de C da equação quadratica:");
+		float numero03 = sc.nextFloat();
+		
+		double delta = Math.pow(numero02,2.0)-4*numero01*numero03;
+		System.out.println(delta);
+		sc.close();
+		if (delta>0){
+			System.out.println("Possui duas raizes");
+		}else if (delta == 0) {
+		System.out.println("Possui uma raizes");
+	}else {
+		System.out.println("Não Possui  raizes");
+	}
+		
+	}
+	
+	
+}
