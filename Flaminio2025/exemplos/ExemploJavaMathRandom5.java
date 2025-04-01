329c7ade58ae03c71adedaed2bd49336130cbc58
Francisco E R Rodrigues <163360208+FranciscoERodrigues@users.noreply.github.com>
2025-04-01 20:42:54 -0300

    Add files via upload

---
+++ Flaminio2025/exemplos/ExemploJavaMathRandom5.java
@@ -0,0 +1,19 @@
+package exemplos;
+
+import java.util.Scanner;
+
+public class ExemploJavaMathRandom5 {
+
+	
+	public static void main(String[] args) {
+		
+	Scanner sc = new Scanner(System.in);
+	
+	
+	System.out.println("o numero aleatorio é: " + (int)(Math.random()*101));
+	
+	sc.close();
+}
+	
+
+}
