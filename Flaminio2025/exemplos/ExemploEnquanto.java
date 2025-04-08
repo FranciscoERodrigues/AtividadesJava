419ddca0cbd96f544595fad5f69eb98320bba2ae
Francisco E R Rodrigues <163360208+FranciscoERodrigues@users.noreply.github.com>
2025-04-08 20:12:21 -0300

    Add files via upload

---
+++ Flaminio2025/exemplos/ExemploEnquanto.java
@@ -0,0 +1,19 @@
+package exemplos;
+
+public class ExemploEnquanto {
+
+	public static void main(String[] args) {
+
+			
+			int contadora = 0;
+			
+			while(contadora < 10) {
+				
+				System.out.println("Francisco");
+				
+				contadora = contadora+1;
+			}
+
+	}
+
+}
