a7cb50623d8654f203149eac78be4981c57e9090
Francisco E R Rodrigues <163360208+FranciscoERodrigues@users.noreply.github.com>
2025-04-15 20:05:56 -0300

    Update Contador1A100.java

--- Flaminio2025/atividade11/Contador1A100.java
+++ Flaminio2025/atividade11/Contador1A100.java
@@ -1,18 +1,20 @@
 package atividade11;
 
-import java.util.Scanner;
+//1) Calcule a soma dos números de 1 a 100.
 
 public class Contador1A100 {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
+		int acumuladora = 0;
 	
-	for(int contador = 1; contador <101;contador ++) {
+		for(int contador = 1; contador <101;contador ++) {
 		
-		System.out.println(contador);
+		acumuladora += contador;
+			
+		System.out.println(acumuladora);
 		
-		sc.close();
+		
 	}
 
 }
