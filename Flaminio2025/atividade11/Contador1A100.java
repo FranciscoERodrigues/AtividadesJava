292143a5a3b4fbd7ed9408eb55eda0e883cf5316
Francisco E R Rodrigues <163360208+FranciscoERodrigues@users.noreply.github.com>
2025-04-15 19:25:11 -0300

    Add files via upload

---
+++ Flaminio2025/atividade11/Contador1A100.java
@@ -0,0 +1,20 @@
+package atividade11;
+
+import java.util.Scanner;
+
+public class Contador1A100 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+	
+	for(int contador = 1; contador <101;contador ++) {
+		
+		System.out.println(contador);
+		
+		sc.close();
+	}
+
+}
+	
+}
