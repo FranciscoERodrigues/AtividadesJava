a3b96913ddae17cf8bd38aaf7dcd040de304e506
Francisco E R Rodrigues <163360208+FranciscoERodrigues@users.noreply.github.com>
2025-04-16 19:17:28 -0300

    Update Fatorial.java

--- Flaminio2025/atividade11/Fatorial.java
+++ Flaminio2025/atividade11/Fatorial.java
@@ -6,7 +6,7 @@ public class Fatorial {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Digite um número inteiro para calcular o fatorial: ");
+        System.out.println("Digite um número inteiro para calcular o fatorial: ");
         int numero = sc.nextInt();
 
         long fatorial = 1;
