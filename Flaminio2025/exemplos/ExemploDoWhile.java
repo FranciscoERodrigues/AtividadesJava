4c071d30792d249878626ba09542650c7e1eaaf0
Francisco E R Rodrigues <163360208+FranciscoERodrigues@users.noreply.github.com>
2025-04-22 19:16:56 -0300

    Add files via upload

---
+++ Flaminio2025/exemplos/ExemploDoWhile.java
@@ -0,0 +1,17 @@
+package exemplos;
+
+public class ExemploDoWhile {
+
+	public static void main(String[] args) {
+	
+		int contadora = 0;
+		
+		do {
+			
+			System.out.println("Francisco");
+			contadora++;
+		}while(contadora<10);
+
+	}
+
+}
