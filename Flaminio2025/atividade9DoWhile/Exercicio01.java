001fa06baa81d799cefa5685376a63a5ace8d1f9
Francisco E R Rodrigues <163360208+FranciscoERodrigues@users.noreply.github.com>
2025-04-22 19:11:18 -0300

    Add files via upload

---
+++ Flaminio2025/atividade9DoWhile/Exercicio01.java
@@ -0,0 +1,17 @@
+package atividade9DoWhile;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+
+		int contadora = 0;
+		
+		do {
+		
+			System.out.println(contadora);
+			contadora++;
+			
+		}while(contadora<11);
+	}
+	
+}
