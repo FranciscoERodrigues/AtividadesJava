040494db51a8d1d9034ab232bc2fb9e84847a14d
Francisco E R Rodrigues <163360208+FranciscoERodrigues@users.noreply.github.com>
2025-04-22 21:19:18 -0300

    Add files via upload

---
+++ Flaminio2025/atividade11/Exercicio07.java
@@ -0,0 +1,21 @@
+package atividade11;
+
+//7) Conte e imprima quantos números divisíveis por 3 existem entre 1 e 50.
+
+public class Exercicio07 {
+
+	public static void main(String[] args) {
+		
+		int contador = 0;
+		
+		for (int i = 0; i < 51; i++) {
+			
+			if(i%3==0) {
+				
+				contador++; 
+				
+			}
+		}
+		System.out.println("A quantidade de numeros que são divisiveis por 3 é:" + contador);		
+	}
+}
