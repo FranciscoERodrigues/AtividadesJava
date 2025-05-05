84e6f406ecdc560b8cb61d27b24827ccc4e1f646
Francisco E R Rodrigues <163360208+FranciscoERodrigues@users.noreply.github.com>
2025-05-05 20:16:20 -0300

    Add files via upload

---
+++ Flaminio2025/metodos/ExemploMetodo1.java
@@ -0,0 +1,15 @@
+package metodos;
+
+public class ExemploMetodo1 {
+
+	public static void main(String[] args) {
+		
+		imprimirNome("Francisco");
+		imprimirNome("Rodrigues");
+	}
+
+public static void imprimirNome(String nome) {
+	System.out.println(nome);
+}
+
+}
---
+++ Flaminio2025/metodos/ExemploMetodo2.java
@@ -0,0 +1,19 @@
+package metodos;
+
+public class ExemploMetodo2 {
+	
+	static void sobrenome(String nome) {
+		
+		System.out.println(nome + " Rodrigues ");
+	}
+
+	public static void main(String[] args) {
+		
+		sobrenome("Mariana");
+		sobrenome("Francisco");
+		sobrenome("Heloisa");
+
+		
+	}
+
+}
