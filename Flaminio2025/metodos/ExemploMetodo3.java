58281dbd4e0e3fc23d6f99349e8ece66995e1fd0
Francisco E R Rodrigues <163360208+FranciscoERodrigues@users.noreply.github.com>
2025-05-05 21:13:46 -0300

    Add files via upload

---
+++ Flaminio2025/metodos/ExemploMetodo3.java
@@ -0,0 +1,24 @@
+package metodos;
+
+public class ExemploMetodo3 {
+
+	
+	public static void exibirDados(String nome,int idade, String CEP){
+		
+		System.out.println("Nome:"+ nome);
+		System.out.println("A idade é:" + idade);
+		System.out.println("O CEP é:" + CEP);
+		
+		
+	
+	}
+	
+
+	public static void main(String[] args) {
+		
+		exibirDados("Yuri", 18, "02873020");
+
+		
+	}
+
+}
