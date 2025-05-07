7cd080c1d82ed5f5d71aebc50d4560871cbfc808
Francisco E R Rodrigues <163360208+FranciscoERodrigues@users.noreply.github.com>
2025-05-06 21:19:23 -0300

    Add files via upload

---
+++ Flaminio2025/metodos/ExemploMetodo6.java
@@ -0,0 +1,29 @@
+package metodos;
+
+
+public class ExemploMetodo6 {
+	
+	public static void VerificarIdade(int idade){
+		
+		
+		if(idade < 18) {
+			
+			System.out.println(" Acesso recusado ");
+		}else {
+			
+			System.out.println( "Acesso Liberado");
+			
+		}
+		
+		
+		
+		
+	}
+
+	public static void main(String[] args) {
+ 
+			VerificarIdade(18);
+
+	}
+
+}
