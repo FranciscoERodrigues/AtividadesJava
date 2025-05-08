5ced09826816fa04f946b96ee5abf91356f3e1db
Francisco E R Rodrigues <163360208+FranciscoERodrigues@users.noreply.github.com>
2025-05-08 19:39:54 -0300

    Add files via upload

---
+++ Flaminio2025/metodos/ExemploMetodo7.java
@@ -0,0 +1,16 @@
+package metodos;
+
+public class ExemploMetodo7 {
+
+	
+	static int myMethod(int x) {
+	    return 5 + x;
+	  }
+	
+	public static void main(String[] args) {
+	
+		  System.out.println(myMethod(5));
+
+	}
+
+}
