d1f70d985e2db16687e846003f93aeccaa249140
Francisco E R Rodrigues <163360208+FranciscoERodrigues@users.noreply.github.com>
2025-05-26 15:21:05 -0300

    Update EquacaoQuadratica.java

--- Flaminio2025/atividade2/EquacaoQuadratica.java
+++ Flaminio2025/atividade2/EquacaoQuadratica.java
@@ -4,30 +4,20 @@ import java.util.Scanner;
 
 
 public class EquacaoQuadratica {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Digite o valor de A da equação quadratica:");
-		float numero01 = sc.nextFloat();
-		System.out.println("Digite o valor de B da equação quadratica:");
-		double numero02 = sc.nextDouble();
-		System.out.println("Digite o valor de C da equação quadratica:");
-		float numero03 = sc.nextFloat();
-		
-		double delta = Math.pow(numero02,2.0)-4*numero01*numero03;
-		System.out.println(delta);
-		sc.close();
-		if (delta>0){
-			System.out.println("Possui duas raizes");
-		}else if (delta == 0) {
-		System.out.println("Possui uma raizes");
-	}else {
-		System.out.println("Não Possui raizes");
-	}
-		
-	}
-	
-	
+        System.out.println("Digite o valor de A: ");
+        double a = sc.nextDouble();
+
+        System.out.println("Digite o valor de B: ");
+        double b = sc.nextDouble();
+
+        System.out.println("Digite o valor de C: ");
+        double c = sc.nextDouble();
+
+        Double delta = b * b - 4 * a * c;
+
+        System.out.println("O resultado é"+ delta);
+    }
 }
