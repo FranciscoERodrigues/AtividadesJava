ebea8679f273ab424d1c6d395409f1ccc03c4b83
Francisco E R Rodrigues <163360208+FranciscoERodrigues@users.noreply.github.com>
2025-05-27 20:42:23 -0300

    Add files via upload

---
+++ Flaminio2025/exemplos/EstruturaDadosExemplo2.java
@@ -0,0 +1,21 @@
+package exemplos;
+
+import java.util.ArrayList;
+
+public class EstruturaDadosExemplo2 {
+
+	public static void main(String[] args) {
+
+		ArrayList<String>frutas = new ArrayList<String>();
+		
+		frutas.add("Maça");
+		frutas.add("Limão");
+		frutas.add("Banana");
+		frutas.add("Uva");
+		frutas.add("Melão");
+
+
+		System.out.println(frutas);
+	}
+
+}
