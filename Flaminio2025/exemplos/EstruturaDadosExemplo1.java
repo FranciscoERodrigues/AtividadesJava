2c8f1dca29be17b3bb6f5a064b94d6cfb90bf5b8
Francisco E R Rodrigues <163360208+FranciscoERodrigues@users.noreply.github.com>
2025-05-27 20:34:41 -0300

    Add files via upload

---
+++ Flaminio2025/exemplos/EstruturaDadosExemplo1.java
@@ -0,0 +1,21 @@
+package exemplos;
+
+import java.util.ArrayList;
+
+public class EstruturaDadosExemplo1 {
+
+	public static void main(String[] args) {
+
+		ArrayList<String>cars = new ArrayList<String>();
+		
+		cars.add("Volvo");
+		cars.add("Porche");
+		cars.add("BMW");
+		cars.add("volkswagen");
+		cars.add("Mercedes");
+		cars.add("volkswagen");
+
+		System.out.println(cars);
+	}
+
+}
