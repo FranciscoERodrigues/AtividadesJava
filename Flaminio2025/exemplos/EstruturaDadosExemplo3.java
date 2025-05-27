888d89c3ffcf524be736ef9165354f2894457efd
Francisco E R Rodrigues <163360208+FranciscoERodrigues@users.noreply.github.com>
2025-05-27 20:50:47 -0300

    Add files via upload

---
+++ Flaminio2025/exemplos/EstruturaDadosExemplo3.java
@@ -0,0 +1,21 @@
+package exemplos;
+
+import java.util.LinkedList;
+
+public class EstruturaDadosExemplo3 {
+
+	public static void main(String[] args) {
+		
+		LinkedList<String> cores = new LinkedList<String>();
+		
+		cores.add("Azul");
+		cores.add("Amarelo");
+		cores.add("Branco");
+		cores.add("Roxo");
+		cores.add("Marrom");
+		
+		System.out.println(cores);
+
+	}
+
+}
