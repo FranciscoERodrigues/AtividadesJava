6576b44331c2e0244b8b59dec06ac648ca94d006
Francisco E R Rodrigues <163360208+FranciscoERodrigues@users.noreply.github.com>
2025-05-27 21:03:42 -0300

    Add files via upload

---
+++ Flaminio2025/exemplos/EstruturaDadosExemplo4.java
@@ -0,0 +1,23 @@
+package exemplos;
+
+import java.util.LinkedList;
+
+public class EstruturaDadosExemplo4 {
+
+	public static void main(String[] args) {
+		
+		LinkedList<String> diasSemana = new LinkedList<String>();
+		
+		diasSemana.add("Domingo");
+		diasSemana.add("Segunda");
+		diasSemana.add("Terça");
+		diasSemana.add("Quarta");
+		diasSemana.add("Quinta");
+		diasSemana.add("Sexta");
+		diasSemana.add("Sabado");
+		
+		System.out.println(diasSemana);
+
+	}
+
+}
